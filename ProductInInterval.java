4bad4a82811f710339c28b33008f9fb080101923
kolevp <33053754+kolevp@users.noreply.github.com>
2017-10-25 09:54:27 +0300

    Add files via upload

---
+++ ProductInInterval.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ProductInInterval {
+			
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+	    int n = sc.nextInt();
+		int m = sc.nextInt();
+		int num = n;
+		long product = 1;
+		do{
+			product *= num;
+			num++;
+		}while(num<=m);
+		System.out.println("Product [n...m] = " + product);
+	}
+
+}
